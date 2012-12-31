5e1f341bc479b3df92ac96f4208f7a21c31004ab
Brian Tomlinson <darthlukan@gmail.com>
2012-12-31 16:50:33 -0600

    Added basic class structure and proposed methods

---
+++ drivel_app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import httplib2
+import pprint
+import multiprocessing
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.client import OAuth2WebServerFlow
+
+class Setup(object):
+    pass
+
+class Login(object):
+    pass
+
+class Files(object):
+    
+    def __init__(self):
+        pass
+    
+    def get_file(self):
+        pass
+    
+    def get_list(self):
+        pass
+    
+    def insert(self):
+        pass
+    
+    def copy(self):
+        pass
+    
+    def remove(self):
+        pass
+    
+    def restore(self):
+        pass
+    
+
+class DriveApp(object):
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ drivel_gui.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import gtk
+import pygtk
+pygtk.require("2.0")
+import multiprocessing
+
+from subprocess import *
+
+class SysTrayApp(object):
+    pass
+
+class MainGui(object):
+    pass
+
+if __name__ == '__main__':
+    pass
