3a620e89c88e2231eb7bd2dbe1ca8a05dd4fe773
Brian Tomlinson <darthlukan@gmail.com>
2012-12-31 23:21:27 -0600

    Copied in some basic stuff from quickstart.py, added initial logic for setting the /home/darthlukan/Drive directory, added TODO comments.

--- drivel_app.py
+++ drivel_app.py
@@ -1,23 +1,70 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import httplib2
+import os
+import pwd
 import pprint
+import httplib2
 import multiprocessing
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
 
 class Setup(object):
-    pass
+    
+    def __init__(self):
+        # Copy your credentials from the APIs Console
+        # TODO: This should be in a config file and hashed, then decoded
+        # at runtime for security purposes.
+        self.CLIENT_ID = 'YOUR_CLIENT_ID'
+        self.CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+
+        # Check https://developers.google.com/drive/scopes for all available scopes
+        self.OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+        # Redirect URI for installed apps
+        self.REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+        
+        # os.getlogin() is the preferred option, but is not working in Peppermint dev env.
+        self.driveDir = '/home/' + pwd.getpwuid(os.getuid()).pw_name + '/Drive/'
+        
+        # Create an httplib2.Http object and authorize it with our credentials
+        self.http = httplib2.Http()
+        self.http = credentials.authorize(self.http)
+
+        self.drive_service = build('drive', 'v2', http=self.http)
+        
+    def get_scope(self):
+        return self.OAUTH_SCOPE
+        
+    def get_redirect(self):
+        return self.REDIRECT_URI
+
+    def get_drive_dir(self):
+        return self.driveDir
+        
+    def set_drive_dir(self):
+        if os.path.isdir(self.driveDir):
+            return self.driveDir
+        else:
+            os.mkdir(self.driveDir)
+            return self.driveDir
 
 class Login(object):
-    pass
+    
+    def __init__(self):
+        # TODO: Modify to match the code layout in my head. Also, tie to GUI.
+        # Run through the OAuth flow and retrieve credentials
+        self.flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+        self.authorize_url = self.flow.step1_get_authorize_url()
+        print 'Go to the following link in your browser: ' + self.authorize_url
+        self.code = raw_input('Enter verification code: ').strip()
+        self.credentials = self.flow.step2_exchange(self.code)
 
 class Files(object):
     
     def __init__(self):
-        pass
+        self.types = {'': ''}
     
     def get_file(self):
         pass
@@ -26,8 +73,17 @@ class Files(object):
         pass
     
     def insert(self):
-        pass
-    
+        # Insert a file (From quickstart)
+        # TODO: This is meant to be gleaned from the GUI.
+        media_body = MediaFileUpload(FILENAME, mimetype='text/plain', resumable=True)
+        body = {
+            'title': 'My document',
+            'description': 'A test document',
+            'mimeType': 'text/plain'
+        }
+        
+        file = drive_service.files().insert(body=body, media_body=media_body).execute()
+        
     def copy(self):
         pass
     
@@ -37,6 +93,8 @@ class Files(object):
     def restore(self):
         pass
     
+    def update(self):
+        pass
 
 class DriveApp(object):
     pass
