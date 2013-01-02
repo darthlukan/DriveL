7db690ee7bb0b59986565086210f4bde9e562b72
Brian Tomlinson <darthlukan@gmail.com>
2013-01-01 18:29:07 -0600

    added more tests that pass

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 import unittest
 import drivel_app as drivel
 
@@ -8,14 +9,24 @@ class TestDrivelSetup(unittest.TestCase):
     def setUp(self):
         self.drivel = drivel.Setup()
         
+    def test_init_variables(self):
+        self.assertEqual(self.drivel.CLIENT_ID, 'YOUR_CLIENT_ID')
+        self.assertEqual(self.drivel.CLIENT_SECRET, 'YOUR_CLIENT_SECRET')
+        self.assertEqual(self.drivel.OAUTH_SCOPE, 'https://www.googleapis.com/auth/drive')
+        
     def test_set_drive_dir(self):
         self.assertTrue(self.drivel.set_drive_dir())
+        self.assertTrue(os.path.isdir(self.drivel.set_drive_dir()))
         
     def test_get_drive_dir(self):
-        self.assertTrue(self.drivel.get_drive_dir())
+        self.assertTrue(os.path.isdir(self.drivel.get_drive_dir()))
         
     def test_get_redirect(self):
         self.assertEqual(self.drivel.get_redirect(), 'http://localhost')
+        
+    def test_get_scope(self):
+        self.assertEqual(self.drivel.get_scope(), 'https://www.googleapis.com/auth/drive')
+        
         
 if __name__ == '__main__':
     unittest.main()
